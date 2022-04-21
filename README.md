170a3d410d932f1d8bcb240e65ed9c51eb9627f7
aman2430 <pendingresult10@gmail.com>
2022-04-21 15:12:03 +0530

    kellton ecommerce

---
+++ README.md
@@ -0,0 +1,8 @@
+# KELLTON CLOTHING STORE
+clothing website with deifferent categories
+-shop products
+-Add to cart
+-Sign up with google
+-sign in
+-Checkout
+-Payment gateway
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import HomePage from "./pages/HomePage/homepage.component";
+import {Route,Routes} from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <Routes>
+          <Route path='/' element={<HomePage/>} />
+          
+      </Routes>
+    </>
+  )
+}
+
+export default  App;
